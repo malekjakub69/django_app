b24b856db1af8e385955db28d646d462db4408af
Jacob Malek <malekjakub69@gmail.com>
2024-01-21 10:57:02 +0100

    finish commit

--- django_app/models.py
+++ django_app/models.py
@@ -3,16 +3,35 @@ from django.db import models
 
 class AttributeName(models.Model):
     nazev = models.CharField(max_length=255, null=True)
-    zobrazit = models.BooleanField()
+    zobrazit = models.BooleanField(null=True)
+    kod = models.CharField(max_length=255, null=True)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "nazev": self.nazev,
+            "zobrazit": self.zobrazit,
+            "kod": self.kod,
+        }
 
 
 class AttributeValue(models.Model):
     hodnota = models.CharField(max_length=255)
 
+    def to_dict(self):
+        return {"id": self.id, "hodnota": self.hodnota}
+
 
 class Attribute(models.Model):
     nazev_atributu = models.ForeignKey(AttributeName, on_delete=models.CASCADE)
     hodnota_atributu = models.ForeignKey(AttributeValue, on_delete=models.CASCADE)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "nazev_atributu": self.nazev_atributu.to_dict(),
+            "hodnota_atributu": self.hodnota_atributu.to_dict(),
+        }
 
 
 class Product(models.Model):
@@ -23,14 +42,38 @@ class Product(models.Model):
     published_on = models.DateTimeField(null=True, blank=True)
     is_published = models.BooleanField(default=False)
 
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "nazev": self.nazev,
+            "description": self.description,
+            "cena": self.cena,
+            "mena": self.mena,
+            "published_on": self.published_on,
+            "is_published": self.is_published,
+        }
+
 
 class ProductAttributes(models.Model):
     attribute = models.ForeignKey(Attribute, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
 
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "attribute": self.attribute.to_dict(),
+            "product": self.product.to_dict(),
+        }
+
 
 class Image(models.Model):
     obrazek = models.URLField()
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "obrazek": self.obrazek,
+        }
 
 
 class ProductImage(models.Model):
@@ -38,9 +81,26 @@ class ProductImage(models.Model):
     obrazek = models.ForeignKey(Image, on_delete=models.CASCADE)
     nazev = models.CharField(max_length=255)
 
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "product": self.product.to_dict(),
+            "obrazek": self.obrazek.to_dict(),
+            "nazev": self.nazev,
+        }
+
 
 class Catalog(models.Model):
     nazev = models.CharField(max_length=255)
-    obrazek = models.ForeignKey(Image, on_delete=models.CASCADE)
+    obrazek = models.ForeignKey(Image, on_delete=models.CASCADE, null=True)
     products = models.ManyToManyField(Product)
     attributes = models.ManyToManyField(Attribute)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "nazev": self.nazev,
+            "obrazek": self.obrazek.to_dict() if self.obrazek else None,
+            "products": [product.id for product in self.products.all()],
+            "attributes": [attribute.id for attribute in self.attributes.all()],
+        }
--- django_app/views.py
+++ django_app/views.py
@@ -1,3 +1,4 @@
+import re
 from django.http import JsonResponse
 from django.views import View
 from django.shortcuts import get_object_or_404, get_list_or_404
@@ -5,6 +6,7 @@ import json
 
 from django.views.generic.detail import DetailView
 from django.apps import apps
+from django.core.serializers import serialize
 
 from django_app.models import (
     Attribute,
@@ -26,76 +28,151 @@ class BaseView(View):
 class ImportView(View):
     def post(self, request):
         data = json.loads(request.body)
+
         for item in data:
             for key, value in item.items():
                 print(key, value)
+                # AttributeName parser
                 if key == "AttributeName":
                     defaults = {
                         "nazev": value["nazev"] if "nazev" in value else None,
-                        # default value is True
-                        "zobrazit": value["zobrazit"] if "zobrazit" in value else True,
+                        "zobrazit": value["zobrazit"] if "zobrazit" in value else None,
                     }
                     AttributeName.objects.update_or_create(
-                        id=value["id"], defaults=defaults
+                        id=value["id"],
+                        defaults={
+                            key: val for key, val in defaults.items() if val is not None
+                        },
                     )
 
+                # AttributeValue parser
                 elif key == "AttributeValue":
+                    defaults = {
+                        "hodnota": value["hodnota"] if "hodnota" in value else None,
+                    }
                     AttributeValue.objects.update_or_create(
-                        id=value["id"], hodnota=value["hodnota"]
+                        id=value["id"],
+                        defaults={
+                            key: val for key, val in defaults.items() if val is not None
+                        },
                     )
+
+                # Attribute parser
                 elif key == "Attribute":
                     defaults = {
                         "nazev_atributu": AttributeName.objects.get(
                             id=value["nazev_atributu_id"]
+                            if "nazev_atributu_id" in value
+                            else None
                         ),
                         "hodnota_atributu": AttributeValue.objects.get(
                             id=value["hodnota_atributu_id"]
+                            if "hodnota_atributu_id" in value
+                            else None
                         ),
                     }
                     Attribute.objects.update_or_create(
-                        id=value["id"], defaults=defaults
+                        id=value["id"],
+                        defaults={
+                            key: val for key, val in defaults.items() if val is not None
+                        },
                     )
+
+                # Product parser
                 elif key == "Product":
                     defaults = {
-                        "nazev": value["nazev"],
-                        "description": value["description"],
-                        "cena": value["cena"],
-                        "mena": value["mena"],
-                        "published_on": value["published_on"],
-                        "is_published": value["is_published"],
+                        "nazev": value["nazev"] if "nazev" in value else None,
+                        "description": value["description"]
+                        if "description" in value
+                        else None,
+                        "cena": value["cena"] if "cena" in value else None,
+                        "mena": value["mena"] if "mena" in value else None,
+                        "published_on": value["published_on"]
+                        if "published_on" in value
+                        else None,
+                        "is_published": value["is_published"]
+                        if "is_published" in value
+                        else None,
                     }
-                    Product.objects.update_or_create(id=value["id"], defaults=defaults)
+                    Product.objects.update_or_create(
+                        id=value["id"],
+                        defaults={
+                            key: val for key, val in defaults.items() if val is not None
+                        },
+                    )
+
+                # Catalog parser
                 elif key == "Catalog":
                     defaults = {
-                        "nazev": value["nazev"],
-                        "obrazek": value["obrazek"],
-                        "products": Product.objects.filter(
-                            id__in=value["products_ids"]
-                        ),
-                        "attributes": Attribute.objects.filter(
-                            id__in=value["attributes_ids"]
-                        ),
+                        "nazev": value["nazev"] if "nazev" in value else None,
+                        "obrazek": value["obrazek"] if "obrazek" in value else None,
                     }
-                    Catalog.objects.update_or_create(id=value["id"], defaults=defaults)
+                    Catalog.objects.update_or_create(
+                        id=value["id"],
+                        defaults={
+                            key: val for key, val in defaults.items() if val is not None
+                        },
+                    )
+                    products = (
+                        Product.objects.filter(id__in=value["products_ids"])
+                        if "products_ids" in value
+                        else []
+                    )
+                    attributes = (
+                        Attribute.objects.filter(id__in=value["attributes_ids"])
+                        if "attributes_ids" in value
+                        else []
+                    )
+                    for product in products:
+                        Catalog.objects.get(id=value["id"]).products.add(product)
+                    for attribute in attributes:
+                        Catalog.objects.get(id=value["id"]).attributes.add(attribute)
+
+                # ProductAttributes parser
                 elif key == "ProductAttributes":
                     defaults = {
-                        "attribute": Attribute.objects.get(id=value["attribute"]),
-                        "product": Product.objects.get(id=value["product"]),
+                        "attribute": Attribute.objects.get(id=value["attribute"])
+                        if "attribute" in value
+                        else None,
+                        "product": Product.objects.get(id=value["product"])
+                        if "product" in value
+                        else None,
                     }
                     ProductAttributes.objects.update_or_create(
-                        id=value["id"], defaults=defaults
+                        id=value["id"],
+                        defaults={
+                            key: val for key, val in defaults.items() if val is not None
+                        },
                     )
+
+                # Image parser
                 elif key == "Image":
-                    defaults = {"obrazek": value["obrazek"]}
-                    Image.objects.update_or_create(id=value["id"], defaults=defaults)
+                    defaults = {
+                        "obrazek": value["obrazek"] if "obrazek" in value else None
+                    }
+                    Image.objects.update_or_create(
+                        id=value["id"],
+                        defaults={
+                            key: val for key, val in defaults.items() if val is not None
+                        },
+                    )
+
+                # ProductImage parser
                 elif key == "ProductImage":
                     defaults = {
-                        "product": Product.objects.get(id=value["product"]),
-                        "obrazek": Image.objects.get(id=value["obrazek_id"]),
-                        "nazev": value["nazev"],
+                        "product": Product.objects.get(id=value["product"])
+                        if "product" in value
+                        else None,
+                        "obrazek": Image.objects.get(id=value["obrazek_id"])
+                        if "obrazek_id" in value
+                        else None,
+                        "nazev": value["nazev"] if "nazev" in value else None,
                     }
                     ProductImage.objects.update_or_create(
-                        id=value["id"], defaults=defaults
+                        id=value["id"],
+                        defaults={
+                            key: val for key, val in defaults.items() if val is not None
+                        },
                     )
                 else:
                     return JsonResponse({"status": "error", "message": "Neznámý klíč"})
@@ -104,14 +181,23 @@ class ImportView(View):
 
 class DetailModelView(DetailView):
     def get(self, _, model_name):
-        model = apps.get_model("django_app", model_name)
+        try:
+            model = apps.get_model("django_app", model_name)
+        except:
+            return JsonResponse({"error": "Model does not exist"}, status=404)
+
         data = model.objects.all()
-        return JsonResponse({"data": list(data)})
+
+        return JsonResponse({"data": [item.to_dict() for item in data]})
 
 
 class DetailModelWithIdView(View):
     def get(self, _, model_name, id):
-        model = apps.get_model("django_app", model_name)
-        print(model)
-        data = get_object_or_404(model, id)
-        return JsonResponse({"data": json.dumps(data)})
+        try:
+            model = apps.get_model("django_app", model_name)
+        except:
+            return JsonResponse({"error": "Model does not exist"}, status=404)
+
+        data = model.objects.get(id=id)
+
+        return JsonResponse({"data": data.to_dict()})
